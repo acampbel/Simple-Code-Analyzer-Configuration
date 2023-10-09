3ca77b694f79ffcce148d7b13e5769c5e6b910eb
Andy Campbell <47190204+acampbel@users.noreply.github.com>
2023-10-09 15:39:10 -0400

    Update buildfile.m

--- buildfile.m
+++ buildfile.m
@@ -1,1 +1,11 @@
-f
+function plan = buildfile
+import matlab.buildtool.tasks.*;
+
+plan = buildplan;
+
+plan("lint") = CodeIssuesTask(Results="results/issues.sarif");
+plan("clean") = CleanTask;
+
+plan.DefaultTasks = "lint";
+
+end
