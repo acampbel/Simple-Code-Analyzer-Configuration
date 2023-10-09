9f127f3cd335b28cf24df521d02b3923b3a8e2b6
Andy Campbell <47190204+acampbel@users.noreply.github.com>
2023-10-09 15:52:47 -0400

    Update buildfile.m

--- buildfile.m
+++ buildfile.m
@@ -1,11 +1,28 @@
 function plan = buildfile
 import matlab.buildtool.tasks.*;
 
-plan = buildplan;
+plan = buildplan(localfunctions);
 
 plan("lint") = CodeIssuesTask(Results="results/issues.sarif");
+plan("lint").Dependencies = "clean";
+
+plan("strictConfig").Inputs = "codeAnalyzerConfiguration.json";
+plan("strictConfig").Outputs = "resources/codeAnalyzerConfiguration.json";
+
+plan("strict_lint") = CodeIssuesTask(Results="results/issues-strict.sarif");
+plna("strict_lint").Dependencies = "strictConfig";
+
 plan("clean") = CleanTask;
 
-plan.DefaultTasks = "lint";
+plan.DefaultTasks = ["lint" "strict_lint"];
+
+end
+
+functions strictConfigTask(~)
+
+if ~isfolder("resources")
+    mkdir("resources");
+end
+copyfile("codeAnalyzerConfiguration.json","resources");
 
 end
